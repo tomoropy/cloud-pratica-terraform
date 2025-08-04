7b31ef4d89b6e2f7be82528b0913435b66bfe8d8
tomoropy <ucchi1910@gmail.com>
2025-08-04 10:44:26 +0900

    cp-bastion-stgをimport！

--- modules/aws/iam_role/main.tf
+++ modules/aws/iam_role/main.tf
@@ -24,3 +24,26 @@ resource "aws_iam_role_policy_attachment" "slack_metrics_backend" {
   policy_arn = each.value
   role       = aws_iam_role.slack_metrics_backend.name
 }
+
+resource "aws_iam_role" "bastion" {
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Sid = ""
+    }]
+    Version = "2012-10-17"
+  })
+  name = "cp-bastion-${var.env}"
+}
+
+resource "aws_iam_role_policy_attachment" "bastion" {
+  for_each = {
+    ssm = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  }
+  policy_arn = each.value
+  role       = aws_iam_role.bastion.name
+}
